6bb5dca5d294c5b79977f3cab20b093d6d48d978
Alexander Kügler <developer.alexan059@gmail.com>
2021-08-08 17:29:33 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dinocalypse 🦖
+
+Work in Progress 🚧
+
+## Game Storyboard
+
+A lonely Dino in a jungle is running away from metor ball.
+He has to run & jump obstacles or he will be slown down and the
+meteor ball will catch and kill him.
+
+The dino can find items on the way to make him faster
+for a short period of time.
--- index.js
+++ index.js
@@ -179,25 +179,3 @@ function runGame() {
 
 
 
-// Game Storyboard
-
-/**
- * A lonely Dino in a jungle is running away from metor ball.
- * He has to run & jump obstacles or he will be slown down and the
- * meteor ball will catch and kill him.
- * 
- * The dino can find items on the way to make him faster
- * for a short period of time.
- */
-
-// Lecture Structure
-
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
